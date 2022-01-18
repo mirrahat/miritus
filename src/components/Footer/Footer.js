173e2613bc6b4385e6d17aabf2ba4fc24f196f09
Md. Yeasir Arafat <yeasirarafatzim@gmail.com>
2022-01-18 16:44:36 +0600

    footer section added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,10 +7,10 @@ import insta from '../../assets/home/insta.png';
 const Footer = () =>{
     return (
         <>
-            <div style={{backgroundColor:'#2b2b2b', paddingTop:'89px', paddingBottom:'120px'}}>
+            <div style={{backgroundColor:'#2b2b2b', paddingTop:'89px', paddingBottom:'90px'}}>
                 <div className="container" >
                     <div className="row">
-                        <div className="col-md-3">
+                        <div className={`col-lg-3 col-sm-6 ${styles.contain}`}>
                             <p className={styles.logo}>Miritus</p>
                             <p className={styles.text}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed</p>
                             <p className={styles.text}>Lorem ipsum dolor sit amet</p>
@@ -22,7 +22,7 @@ const Footer = () =>{
                             </div>
                         </div>
 
-                        <div className="col-md-3">
+                        <div className={`col-lg-3 col-sm-6 ${styles.contain}`}>
                             <p className={styles.head}>Services</p>
                             <p className={styles.text}>4517 Washington Ave. Manchester, Kentucky 39495</p>
                             <p className={styles.text}>deanna.curtis@example.com</p>
@@ -30,14 +30,14 @@ const Footer = () =>{
 
                         </div>
 
-                        <div className="col-md-3">
+                        <div className={`col-lg-3 col-sm-6 ${styles.contain}`}>
                             <p className={styles.head}>Head Office</p>
                             <p className={styles.text}>4517 Washington Ave. Manchester, Kentucky 39495</p>
                             <p className={styles.text}>deanna.curtis@example.com</p>
                             <p className={styles.text}>(671) 555-0110</p>
                         </div>
 
-                        <div className="col-md-3">
+                        <div className={`col-lg-3 col-sm-6 ${styles.contain}`}>
                             <p className={styles.head}>Opening Hour</p>
                             <p className={styles.text}>Mon - Sat : 8:00 - 17:00</p>
                             <p className={styles.text}>Sun : Closed</p>

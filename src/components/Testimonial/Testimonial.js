22d76c3f7122c374cbbd11db4e3968204c50e7d5
Md. Yeasir Arafat <yeasirarafatzim@gmail.com>
2022-01-26 23:10:11 +0600

    testimonial completed

--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -1,26 +1,30 @@
 import React from "react";
+import './style.css';
 import styles from './Testimonial.module.css';
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
-import image from '../../assets/home/testimonial1.png';
+import { fakeData } from "./fakeData";
 
 function Testimonial() {
     const responsive = {
         desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3,
-          slidesToSlide: 1 // optional, default to 1.
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3,
+            slidesToSlide: 2, // optional, default to 1. -35
+
         },
         tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2,
-          slidesToSlide: 2 // optional, default to 1.
+            breakpoint: { max: 1024, min: 560 },
+            items: 2,
+            slidesToSlide: 2, // optional, default to 1.
+
         },
         mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1,
-          slidesToSlide: 1 // optional, default to 1.
+            breakpoint: { max: 560, min: 0 },
+            items: 1.5,
+            slidesToSlide: 1, // optional, default to 1.
+
         }
       };
   
@@ -37,95 +41,36 @@ function Testimonial() {
                 ssr={true} // means to render carousel on server-side.
                 infinite={true}
                 autoPlay={true}
-                // centerMode={true}
                 autoPlaySpeed={3000}
-                keyBoardControl={true}
-                customTransition="transform 300ms ease-in-out"
-                transitionDuration={1000}
+                keyBoardControl={false}
+                customTransition="transform 400ms ease-in-out"
+                transitionDuration={500}
                 containerClass={`carousel-container`}
                 arrows={false}
-                dotListClass="custom-dot-list-style"
-                itemClass={`carousel-item-padding-40-px mx-2`}
-                minimumTouchDrag={50}
-                centerMode={true}
+                dotListClass={`custom-dot-list-style`}
+                itemClass={`carousel-item-padding-40-px mx-2 d-flex align-items-stretch`}
+                partialVisbile={false}
+                // additionalTransfrom={-50}
                 >
+                {
+                    fakeData.map((x) =>(
+                        <div className={styles.box} key={x.id}>
+                            <div className="row">
+                                <div className="col-md-3 col-sm-3">
+                                    <img src={x.img} alt="" className={styles.img}/>
+                                </div>
+                                <div className="col-md-9 col-sm-9">
+                                    <p className={styles.name}>{x.name}</p>
+                                    <p className={styles.profession}>{x.profession}</p>
+                                </div>                
+                            </div>
 
-                <div className={styles.box}>
-                    <div className="row">
-                        <div className="col-md-3">
-                            <img src={image} alt="" className={styles.img}/>
+                            <div className="row">
+                                <p className={styles.msg}>{x.message}</p>
+                            </div>
                         </div>
-                        <div className="col-md-9">
-                            <p className={styles.name}>Howard Arlene</p>
-                            <p className={styles.profession}>Finance Manager</p>
-                        </div>                
-                    </div>
-
-                    <div className="row">
-                        <p className={styles.msg}>Your company is truly upstanding and is behind its product 100%. It's the perfect solution for our business. It has really helped our business.</p>
-                    </div>
-                </div>
-
-                <div className={styles.box}>
-                    <div className="row">
-                        <div className="col-md-3">
-                            <img src={image} alt="" className={styles.img}/>
-                        </div>
-                        <div className="col-md-9">
-                            <p className={styles.name}>Miles Esther</p>
-                            <p className={styles.profession}>Public Relations Specialist</p>
-                        </div>                
-                    </div>
-
-                    <div className="row">
-                        <p className={styles.msg}>Your company is truly upstanding and is behind its product 100%. It's the perfect solution for our business. It has really helped our business.</p>
-                    </div>
-                </div>
-                <div className={styles.box}>
-                    <div className="row">
-                        <div className="col-md-3">
-                            <img src={image} alt="" className={styles.img}/>
-                        </div>
-                        <div className="col-md-9">
-                            <p className={styles.name}>Howard Arlene</p>
-                            <p className={styles.profession}>Finance Manager</p>
-                        </div>                
-                    </div>
-
-                    <div className="row">
-                        <p className={styles.msg}>Your company is truly upstanding and is behind its product 100%. It's the perfect solution for our business. It has really helped our business.</p>
-                    </div>
-                </div>
-                <div className={styles.box}>
-                    <div className="row">
-                        <div className="col-md-3">
-                            <img src={image} alt="" className={styles.img}/>
-                        </div>
-                        <div className="col-md-9">
-                            <p className={styles.name}>Howard Arlene</p>
-                            <p className={styles.profession}>Finance Manager</p>
-                        </div>                
-                    </div>
-
-                    <div className="row">
-                        <p className={styles.msg}>Your company is truly upstanding and is behind its product 100%. It's the perfect solution for our business. It has really helped our business.</p>
-                    </div>
-                </div>
-                <div className={styles.box}>
-                    <div className="row">
-                        <div className="col-md-3">
-                            <img src={image} alt="" className={styles.img}/>
-                        </div>
-                        <div className="col-md-9">
-                            <p className={styles.name}>Howard Arlene</p>
-                            <p className={styles.profession}>Finance Manager</p>
-                        </div>                
-                    </div>
-
-                    <div className="row">
-                        <p className={styles.msg}>Your company is truly upstanding and is behind its product 100%. It's the perfect solution for our business. It has really helped our business.</p>
-                    </div>
-                </div>
+                    ))
+                }
                 
 
             </Carousel>

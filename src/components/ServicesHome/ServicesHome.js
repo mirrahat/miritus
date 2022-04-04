230a8bc8705cdefcde0f4631d77b6dd41d54534e
Md. Yeasir Arafat <yeasirarafatzim@gmail.com>
2022-04-04 22:40:45 +0600

    Testimonial, Services bug fixed

--- src/components/ServicesHome/ServicesHome.js
+++ src/components/ServicesHome/ServicesHome.js
@@ -1,26 +1,38 @@
 import React from "react";
-import styles from './ServicesHome.module.css';
+import styles from "./ServicesHome.module.css";
 import ServiceHome from "../ServiceHome/ServiceHome";
-import data from './data';
+import data from "./data";
 
 const ServicesHome = () => {
-    return(
+    return (
         <div className={`container ${styles.contain} overflow-hidden pb-4`}>
-                <p data-aos='slide-right' className={styles.heading}>Services</p>
+            <p data-aos="slide-right" className={styles.heading}>
+                Services
+            </p>
             <div>
-                <p data-aos='slide-left' className={`mx-auto ${styles.subHeading}`}>Modernize your workflow and maximize result</p>
+                <p
+                    data-aos="slide-left"
+                    className={`mx-auto ${styles.subHeading}`}
+                >
+                    Modernize your workflow and maximize result
+                </p>
             </div>
 
             <div className="row">
-                {
-                    data && data.map(x=>(
+                {data &&
+                    data.map((x) => (
                         <div key={x.id} className="col-lg-4 col-md-6">
-                            <ServiceHome id={x.id} heading={x.heading} content={x.content} to={x.to} icon={x.icon}/>
+                            <ServiceHome
+                                id={x.id}
+                                heading={x.heading}
+                                content={x.content}
+                                to={x.to}
+                                icon={x.icon}
+                            />
                         </div>
-                    ))
-                }
+                    ))}
             </div>
         </div>
-    )
-}
+    );
+};
 export default ServicesHome;

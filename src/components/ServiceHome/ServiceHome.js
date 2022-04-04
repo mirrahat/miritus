cc92a4cbb9e4edb4ca20e79179186a14c5d078dc
Md. Yeasir Arafat <46840507+YeasirArafatZim@users.noreply.github.com>
2022-04-04 22:41:35 +0600

    Testimonial, Services bug fixed (#13)

--- src/components/ServiceHome/ServiceHome.js
+++ src/components/ServiceHome/ServiceHome.js
@@ -1,19 +1,22 @@
 import React from "react";
-import styles from './ServiceHome.module.css';
-import { Link } from 'react-router-dom'
-import { HiOutlineArrowNarrowRight } from 'react-icons/hi';
+import styles from "./ServiceHome.module.css";
+import { Link } from "react-router-dom";
+import { HiOutlineArrowNarrowRight } from "react-icons/hi";
 
-const ServiceHome = (props) =>{
-    
-    return(
-        <div className={styles.box}>
-            <img src={props.icon} alt="" className={styles.img}/>
+const ServiceHome = (props) => {
+    return (
+        <div className={`${styles.box} user-select-none`}>
+            <img src={props.icon} alt="" className={styles.img} />
             <p className={styles.heading}>{props.heading}</p>
             <p className={styles.text}>{props.content}</p>
             <Link to={`/individualService?id=${props.id}`}>
-                <HiOutlineArrowNarrowRight className={styles.to} size="2rem" color="#ff4c0a"/>
+                <HiOutlineArrowNarrowRight
+                    className={styles.to}
+                    size="2rem"
+                    color="#ff4c0a"
+                />
             </Link>
         </div>
-    )
-}
+    );
+};
 export default ServiceHome;
--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -1,6 +1,6 @@
 import React from "react";
-import './style.css';
-import styles from './Testimonial.module.css';
+import "./style.css";
+import styles from "./Testimonial.module.css";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
@@ -10,28 +10,29 @@ function Testimonial() {
     const responsive = {
         desktop: {
             breakpoint: { max: 3000, min: 1024 },
-            items: 3,
+            items: 2,
             slidesToSlide: 2, // optional, default to 1. -35
-
         },
         tablet: {
             breakpoint: { max: 1024, min: 560 },
-            items: 2,
-            slidesToSlide: 2, // optional, default to 1.
-
+            items: 1,
+            slidesToSlide: 1, // optional, default to 1.
         },
         mobile: {
             breakpoint: { max: 560, min: 0 },
-            items: 1.5,
+            items: 0.6,
             slidesToSlide: 1, // optional, default to 1.
+        },
+    };
 
-        }
-      };
-  
     return (
         <div className={`${styles.container} overflow-hidden`}>
-            <p data-aos='slide-right' className={styles.head}>Our Testimonial</p>
-            <p data-aos='slide-left' className={styles.subHead}>Our trusted client review</p>
+            <p data-aos="slide-right" className={styles.head}>
+                Our Testimonial
+            </p>
+            <p data-aos="slide-left" className={styles.subHead}>
+                Our trusted client review
+            </p>
 
             <Carousel
                 swipeable={true}
@@ -48,35 +49,37 @@ function Testimonial() {
                 containerClass={`carousel-container`}
                 arrows={false}
                 dotListClass={`custom-dot-list-style`}
-                itemClass={`carousel-item-padding-40-px mx-2 d-flex align-items-stretch mt-3`}
-                partialVisbile={false}
-                // additionalTransfrom={-50}
-                >
-                {
-                    fakeData.map((x) =>(
-                        <div className={styles.box} key={x.id}>
-                            <div className="row">
-                                <div className="col-md-3 col-sm-3">
-                                    <img src={x.img} alt="" className={styles.img}/>
-                                </div>
-                                <div className="col-md-9 col-sm-9">
-                                    <p className={styles.name}>{x.name}</p>
-                                    <p className={styles.profession}>{x.profession}</p>
-                                </div>                
+                itemClass={`carousel-item-padding-40-px d-flex align-items-stretch mt-3 px-2`}
+                // focusOnSelect={true}
+                // partialVisbile={true}
+                centerMode={true}
+            >
+                {fakeData.map((x) => (
+                    <div className={styles.box} key={x.id}>
+                        <div className="row">
+                            <div className="col-md-3 col-sm-3">
+                                <img
+                                    src={x.img}
+                                    alt=""
+                                    className={styles.img}
+                                />
                             </div>
-
-                            <div className="row">
-                                <p className={styles.msg}>{x.message}</p>
+                            <div className="col-md-9 col-sm-9">
+                                <p className={styles.name}>{x.name}</p>
+                                <p className={styles.profession}>
+                                    {x.profession}
+                                </p>
                             </div>
                         </div>
-                    ))
-                }
-                
 
+                        <div className="row">
+                            <p className={styles.msg}>{x.message}</p>
+                        </div>
+                    </div>
+                ))}
             </Carousel>
         </div>
-
     );
-  }
-  
+}
+
 export default Testimonial;
